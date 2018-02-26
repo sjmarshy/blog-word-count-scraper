d390ccdfae493cf35ffbffdf9ccc0bbc8a7d374a
Sam Marshall <sjmarshy@gmail.com>
2018-02-26 21:12:22 +0000

    this is a dirty kludge-hack, but hey it's just a script right?

--- index.ts
+++ index.ts
@@ -1,11 +1,17 @@
 import fetch from 'node-fetch';
 import cheerio from 'cheerio';
 
+let latestWordcount = 0;
+
 const update = () =>
     fetch('https://blog.sjm.computer/wordcount')
         .then(req => req.text())
         .then(body => cheerio.load(body))
         .then($ => $('#wordcount').text())
+        .then(value => {
+            latestWordcount = parseInt(value, 10);
+            return value;
+        })
         .then(value =>
             fetch(
                 `https://www.beeminder.com/api/v1/users/samiswellcool/goals/published-words/datapoints.json?auth_token=${
@@ -23,3 +29,5 @@ const update = () =>
 update();
 
 setInterval(update, 1000 * 60 * 60);
+
+export default () => `running...wc ${latestWordcount}`;
